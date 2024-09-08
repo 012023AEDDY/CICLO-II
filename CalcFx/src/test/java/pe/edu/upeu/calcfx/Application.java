3225948b11528b6ac69e3f6709b8c839075ad535
012023AEDDY <maikerjetasahamigua@gmail.com>
2024-09-08 17:50:41 -0500

    Add files via upload

---
+++ CalcFx/src/test/java/pe/edu/upeu/calcfx/Application.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.calcfx;
+
+public class Application {
+    public static void main(String[] args) {
+        Application.main(args);//esta es la encragada de ejecutar todo el proyecto
+    }
+}
